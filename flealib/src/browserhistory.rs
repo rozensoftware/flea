d38c8208d183aa521788ba66106547217710f4e1
Piotr Rozentreter <rozsoft@wp.pl>
2023-02-14 20:08:06 +0100

    Fix for web history output

--- flealib/src/browserhistory.rs
+++ flealib/src/browserhistory.rs
@@ -29,7 +29,7 @@ impl fmt::Display for History
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
     {
-        write!(f, "URL:{};TITLE:{};VISIT COUNT:{}\r\n", &self.url, &self.title, &self.visit_count)
+        write!(f, "URL:{};TITLE:{};VISIT COUNT:{};", &self.url, &self.title, &self.visit_count)
     }
 }
 
