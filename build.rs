6271d5be424ff6f5dc161085fcb8cd904f1ea724
Piotr Rozentreter <rozsoft@wp.pl>
2023-03-15 22:26:22 +0100

    Fix for bash execution on Windows

--- build.rs
+++ build.rs
@@ -4,13 +4,13 @@ use std::env;
 fn main() 
 {
     println!("cargo:rerun-if-changed=src/hideinjector.c");
-    println!("cargo:rerun-if-changed=src/lockscreen.c");
+    println!("cargo:rerun-if-changed=src/helper.c");
     
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .file("src/hideinjector.c")
-        .file("src/lockscreen.c")
-        .compile("hideinjector");
+        .file("src/helper.c")
+        .compile("chelper");
 
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();    
     println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lib").display());
